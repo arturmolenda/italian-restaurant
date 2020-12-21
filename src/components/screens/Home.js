1bf40ad959ce59e4e1b64ff03663603db15c527d
Artur Molenda <ar2rweb@gmail.com>
2020-12-21 08:35:59 +0100

    initial home screen

---
+++ src/components/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='container flex justify-center mx-auto mt-10'>
+      <h1 className='text-3xl font-bold '>
+        Welcome to our restaurant, Grazie!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
