d8b966c02bbcdd9e439a13821fbe8a252f9ccf09
Artur Molenda <ar2rweb@gmail.com>
2020-12-21 15:55:40 +0100

    initial schemas

---
+++ sanity/schemas/images.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'images',
+  title: 'Images',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      description: 'Optional',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      description: 'Best to keep images 1920x1280',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      description: 'Optional',
+      type: 'text',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      description: 'description',
+      image: 'image',
+    },
+    prepare: ({ title, image }) => {
+      return {
+        title: title ? title : '',
+        media: image,
+      };
+    },
+  },
+};
