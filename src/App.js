acbb1a57a1f8131af8b13a85a0fd4e266ccebbf7
Artur Molenda <ar2rweb@gmail.com>
2020-12-23 18:16:11 +0100

    add aboutUs data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import About from './components/screens/About';
 import Home from './components/screens/Home';
 import { StateContext } from './context/context';
 import { useStateContext } from './context/context-hook';
 const App = () => {
-  const { carouselImages } = useStateContext();
+  const { carouselImages, aboutData } = useStateContext();
   return (
-    <StateContext.Provider value={{ carouselImages }}>
+    <StateContext.Provider value={{ carouselImages, aboutData }}>
       <Router>
         <Layout>
           <Switch>
             <Route path='/' exact component={Home} />
+            <Route path='/about' exact component={About} />
           </Switch>
         </Layout>
       </Router>
--- src/context/context-hook.js
+++ src/context/context-hook.js
@@ -3,7 +3,9 @@ import sanityClient from '../client';
 
 export const useStateContext = () => {
   const [carouselImages, setCarouselImages] = useState([]);
+  const [aboutData, setAboutData] = useState({});
   useEffect(() => {
+    // get carouselImages
     sanityClient
       .fetch(
         `*[_type == "carouselImages"]{
@@ -11,18 +13,53 @@ export const useStateContext = () => {
               _id,
               title,
               description,
-              "image": image.asset->url
+              "image": image.asset
             }
           }`
       )
       .then((res) => {
         console.log(res);
-        if (res.length !== 0 && res[0].imagesArray.length !== 0)
+        if (res.length !== 0 && res.length !== 0)
           setCarouselImages(res[0].imagesArray);
       })
       .catch((err) => console.log(err));
+
+    // get aboutData
+    sanityClient
+      .fetch(
+        `*[_type == "aboutUs"]{
+        title,
+        "aboutUsImage": aboutUsImage[0]->{
+          title,
+          description,
+          "image": image.asset
+        },
+     aboutUs,
+     "dishImage": dishImage[0]->{
+       title,
+       description,
+       "image": image.asset
+     },
+     advantage1,
+     advantage2,
+     advantage3,
+     advantage4,
+     "extraImage": extraImage[0]->{
+      title,
+      description,
+      "image": image.asset
+    },
+     }`
+      )
+      .then((res) => {
+        console.log(res);
+        if (res.length !== 0) setAboutData(res[0]);
+      })
+      .catch((err) => console.log(err));
   }, []);
+  console.log(aboutData);
   return {
     carouselImages,
+    aboutData,
   };
 };
